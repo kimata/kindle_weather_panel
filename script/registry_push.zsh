46cf302887004590ef5ce63d2b19640ed2298d2a
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-01 22:57:06 +0900

    fix: typo

--- script/registry_push.zsh
+++ script/registry_push.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-NAME=e-ink_weather_panel
+NAME=kindle_weather_panel
 REGISTRY=registry.green-rabbit.net/library
 
 git push
 docker build . -t ${NAME}
-docker tag e-ink_weather_panel ${REGISTRY}/${NAME}
+docker tag ${NAME} ${REGISTRY}/${NAME}
 docker push ${REGISTRY}/${NAME}
