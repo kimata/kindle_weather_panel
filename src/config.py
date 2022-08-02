e5b9c0bfe512b7d67d025e722277f68686adb075
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-02 23:46:19 +0900

    fix: 設定ファイルのパスを指定できるようにした

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ import os
 CONFIG_PATH = "../config.yml"
 
 
-def load_config():
-    path = str(pathlib.Path(os.path.dirname(__file__), CONFIG_PATH))
+def load_config(config_path=CONFIG_PATH):
+    path = str(pathlib.Path(os.path.dirname(__file__), config_path))
     with open(path, "r") as file:
         return yaml.load(file, Loader=yaml.SafeLoader)
