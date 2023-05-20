00e486d36f7819ab2f7339183a31d92a9da9f1c1
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-20 13:23:40 +0900

    fix: ライブラリを他のプロジェクトに合わせて更新

--- src/notify_slack.py
+++ src/notify_slack.py
@@ -18,9 +18,9 @@ SIMPLE_TMPL = """\
 [
     {{
         "type": "header",
-	"text": {{
+    "text": {{
             "type": "plain_text",
-	    "text": "{title}",
+        "text": "{title}",
             "emoji": true
         }}
     }},
@@ -28,8 +28,8 @@ SIMPLE_TMPL = """\
         "type": "section",
         "text": {{
             "type": "mrkdwn",
-	    "text": {message}
-	}}
+        "text": {message}
+    }}
     }}
 ]
 """
@@ -60,6 +60,8 @@ def send(token, ch_name, message):
 def split_send(token, ch_name, title, message, formatter=format_simple):
     LINE_SPLIT = 20
 
+    logging.info("Post slack channel: {ch_name}".format(ch_name=ch_name))
+
     message_lines = message.splitlines()
     for i in range(0, len(message_lines), LINE_SPLIT):
         send(
@@ -69,8 +71,8 @@ def split_send(token, ch_name, title, message, formatter=format_simple):
         )
 
 
-def info(token, ch_name, message, formatter=format_simple):
-    title = "Info"
+def info(token, ch_name, name, message, formatter=format_simple):
+    title = "Info: " + name
     split_send(token, ch_name, title, message, formatter)
 
 
@@ -89,30 +91,6 @@ def check_interval(interval_min):
     return True
 
 
-def error(
-    token,
-    ch_name,
-    ch_id,
-    message,
-    attatch_img=None,
-    interval_min=10,
-    formatter=format_simple,
-):
-    title = "Error"
-
-    if not check_interval(interval_min):
-        logging.warning("RETURN")
-        return
-
-    split_send(token, ch_name, title, message, formatter)
-
-    if attatch_img is not None:
-        error_img(token, ch_id, title, attatch_img["data"], attatch_img["text"])
-
-    ERROR_NOTIFY_FOOTPRINT.parent.mkdir(parents=True, exist_ok=True)
-    ERROR_NOTIFY_FOOTPRINT.touch()
-
-
 def error_img(token, ch_id, title, img, text):
     client = slack_sdk.WebClient(token=token)
 
@@ -128,14 +106,67 @@ def error_img(token, ch_id, title, img, text):
             logging.warning(e.response["error"])
 
 
+def error(
+    token,
+    ch_name,
+    name,
+    message,
+    interval_min=10,
+    formatter=format_simple,
+):
+    title = "Error: " + name
+
+    if not check_interval(interval_min):
+        logging.warning("Interval is too short. Skipping.")
+        return
+
+    split_send(token, ch_name, title, message, formatter)
+
+    ERROR_NOTIFY_FOOTPRINT.parent.mkdir(parents=True, exist_ok=True)
+    ERROR_NOTIFY_FOOTPRINT.touch()
+
+
+def error_with_image(
+    token,
+    ch_name,
+    ch_id,
+    name,
+    message,
+    attatch_img,
+    interval_min=10,
+    formatter=format_simple,
+):
+    title = "Error: " + name
+
+    if not check_interval(interval_min):
+        logging.warning("Interval is too short. Skipping.")
+        return
+
+    split_send(token, ch_name, title, message, formatter)
+
+    if attatch_img is not None:
+        if ch_id is None:
+            logging.error("ch_id is not specified.")
+
+        error_img(token, ch_id, title, attatch_img["data"], attatch_img["text"])
+
+    ERROR_NOTIFY_FOOTPRINT.parent.mkdir(parents=True, exist_ok=True)
+    ERROR_NOTIFY_FOOTPRINT.touch()
+
+
 if __name__ == "__main__":
+    import os
     import logger
     import sys
     import PIL.Image
     from config import load_config
 
-    logger.init("test", level=logging.WARNING)
+    logger.init("test", level=logging.INFO)
     logging.info("Test")
+
+    # NOTE: Slack の環境に合わせて書き換える
+    test_ch_name = "#test"
+    test_ch_id = "C058PTZG13L"
 
     config = load_config()
     if "SLACK" not in config:
@@ -152,16 +183,27 @@ if __name__ == "__main__":
     if "INFO" in config["SLACK"]:
         info(
             config["SLACK"]["BOT_TOKEN"],
-            config["SLACK"]["INFO"]["CHANNEL"]["NAME"],
+            test_ch_name,
+            os.path.basename(__file__),
             "メッセージ\nメッセージ",
         )
 
     if "ERROR" in config["SLACK"]:
         error(
             config["SLACK"]["BOT_TOKEN"],
-            config["SLACK"]["ERROR"]["CHANNEL"]["NAME"],
-            config["SLACK"]["ERROR"]["CHANNEL"]["ID"],
+            test_ch_name,
+            os.path.basename(__file__),
+            "エラーメッセージ",
+            0,
+        )
+
+    if "ERROR" in config["SLACK"]:
+        error_with_image(
+            config["SLACK"]["BOT_TOKEN"],
+            test_ch_name,
+            test_ch_id,
+            os.path.basename(__file__),
             "エラーメッセージ",
             {"data": img, "text": "エラー時のスクリーンショット"},
-            interval_min=config["SLACK"]["ERROR"]["INTERVAL_MIN"],
+            0,
         )
